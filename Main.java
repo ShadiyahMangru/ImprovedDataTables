189e892af9bbf4f3ff5e03fe9f96e5ca8fe11877
Shadiyah Mangru <shadiyahmangru@gmail.com>
2019-03-22 11:06:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,11 @@ public class Main{
 	
 	public void mainMenu(){
 		System.out.println("\n\t**********************************************************");
-		System.out.println(String.format("%1s %-53s %2s", "\t*", "***** Welcome to Hockey Data Filtering Wizard! *****", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "********** Welcome to Hockey Data Wizard! **********", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "", "*"));
-		System.out.println(String.format("%1s %-53s %2s", "\t*", "     Query Current Roster to Determine Number of:", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "     ENTER THE NUMBER OF YOUR SELECTION", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", " ", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "     Stats Filtered by Position:", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "      1.) Centers", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "      2.) Left-Wingers", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "      3.) Right-Wingers", "*"));
@@ -16,7 +18,11 @@ public class Main{
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "      6.) Defense", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "      7.) Goalies", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", " ", "*"));
-		System.out.println(String.format("%1s %-53s %2s", "\t*", "      8.) Exit", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "     View the Current Roster:", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "      8.) All Players", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", " ", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", " ", "*"));
+		System.out.println(String.format("%1s %-53s %2s", "\t*", "      9.) Exit", "*"));
 		System.out.println(String.format("%1s %-53s %2s", "\t*", "", "*"));
 		System.out.println("\t**********************************************************");
 		Console console = System.console();
@@ -25,9 +31,14 @@ public class Main{
 			userInput = console.readLine();
 			//console.writer().println("Your selection: " + userInput);
 		}
-		if(userInput.equals("8")){
+		if(userInput.equals("9")){
 			System.out.println("You selected: EXIT");
 			System.exit(0);
+		}
+		else if(userInput.equals("8")){
+			System.out.println("You selected: DISPLAY ROSTER");
+			DataTable dt = new DataTable(SortOptions.sortByName, "Goalies", "Goalie");
+			dt.getRoster();
 		}
 		else if(userInput.equals("7")){
 			DataTable dt = new DataTable(SortOptions.sortByName, "Goalies", "Goalie");
@@ -65,4 +76,4 @@ public class Main{
 		m.mainMenu();
 	}
 
-} 
+}
